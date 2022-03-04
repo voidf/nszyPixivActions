46b84675e9898e7d54690f7a9759a1a64491696a
ATRI <A@T.RI>
2022-03-05 00:53:12 +0800

    add: 开个随机看图接口方便网页用户

--- api.py
+++ api.py
@@ -70,9 +70,41 @@ async def get_pending_list(typ: str='ero'):
     }])
     print(r)
     for i in r:
-        print(i)
+        # print(i)
+        i['id'] = i.pop('_id')
 
         return i
+
+@app.get('/randbin')
+async def get_pending_list(typ: str='ero'):
+    """随机拿一张图的二进制
+
+    可以使用typ指定需要的种类：
+
+    - kawaii
+        萝莉涩图
+
+    - ero
+        默认涩图
+
+    - nice
+        完成度高的或者艺术感占主要成分的图
+
+    - r18
+        不带幻影坦克会炸群的图，可能不色但一定是黄图
+
+    - kusa
+        试试就逝世
+    
+    """
+    r = Passed.objects().aggregate([{
+        '$match': { 'typ': typ }
+   },{
+        '$sample':{'size':1}
+    }])
+    print(r)
+    for i in r:
+        return Response(PictureBinary.objects(pk=i['_id']).first().content.read(), media_type='image/jpeg')
 
 if __name__ == '__main__':
     sys.dont_write_bytecode = True
