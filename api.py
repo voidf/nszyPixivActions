00b0b6398f6d26e0c9024e8da9de9ad3cde547af
ATRI <A@T.RI>
2022-03-05 00:21:39 +0800

    add: 面向公众的随机只读api

---
+++ api.py
@@ -0,0 +1,81 @@
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.responses import *
+from fastapi.middleware.cors import CORSMiddleware
+from loguru import logger
+
+import os
+import sys
+
+from datamodel import *
+
+sys.dont_write_bytecode = True
+
+
+app = FastAPI(version="1.0.0", title="setu randomize system")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/bin/{pid}')
+async def get_binary(pid:int):
+    """拿库里给定pid的一张图
+    """
+    r = PictureBinary.objects(pk=pid).first()
+    if r:
+        return Response(r.content.read(), media_type='image/jpeg')
+    else:
+        raise HTTPException(404)
+
+@app.get('/pending/{pid}')
+async def get_pending(pid:int):
+    """拿待审区的图
+    """
+    r = Pending.objects(pk=pid).first()
+    if r:
+        return r.to_dict()
+    else:
+        raise HTTPException(404)
+
+@app.get('/random')
+async def get_pending_list(typ: str='ero'):
+    """
+    随机拿一条图的信息
+
+    可以使用typ指定需要的种类：
+
+    kawaii
+        萝莉涩图
+    ero
+        默认涩图
+    nice
+        完成度高的或者艺术感占主要成分的图
+    r18
+        不带幻影坦克会炸群的图，可能不色但一定是黄图
+    kusa
+        试试就逝世
+    
+    """
+    r = Passed.objects().aggregate([{
+        '$match': { 'typ': typ }
+   },{
+        '$sample':{'size':1}
+    }])
+    print(r)
+    for i in r:
+        print(i)
+
+        return i
+
+if __name__ == '__main__':
+    sys.dont_write_bytecode = True
+    import uvicorn
+    uvicorn.run(
+        app,
+        host='0.0.0.0',
+        port=11002,
+    )
